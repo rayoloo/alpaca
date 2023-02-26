30ef7c634239b0ede038bfae30299d0fecc1bc63
rayoloo <raymond.lam122@gmail.com>
2023-02-25 20:04:50 -0500

    new song

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { BsPauseFill, BsFillPlayFill } from 'react-icons/bs'
 import '../styles/Player.css'
+import audiofile from '../assets/Happy_Upbeat.mp3'
 
 const useAudio = url => {
 	const [audio] = useState(new Audio(url))
@@ -9,6 +10,7 @@ const useAudio = url => {
 	const toggle = () => setPlaying(!playing)
 
 	useEffect(() => {
+		audio.volume = 0.1
 		playing ? audio.play() : audio.pause()
 	}, [playing])
 
@@ -24,7 +26,7 @@ const useAudio = url => {
 
 const Player = () => {
 	const url = 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3'
-	const [playing, toggle] = useAudio(url)
+	const [playing, toggle] = useAudio(audiofile)
 
 	return (
 		<div onClick={toggle}>
