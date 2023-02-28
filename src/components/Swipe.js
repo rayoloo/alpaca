09f7736eeb8ee1866a2742366dfcc19eb44e3cc3
rayoloo <raymond.lam122@gmail.com>
2023-02-28 01:29:44 -0500

    volume change

--- src/components/Swipe.js
+++ src/components/Swipe.js
@@ -2,7 +2,7 @@ import audiofile from '../assets/Swipe.mp3'
 
 const Swipe = () => {
 	const audio = new Audio(audiofile)
-	audio.volume = 0.25
+	audio.volume = 0.1
 	audio.loop = false
 	audio.play()
 }
